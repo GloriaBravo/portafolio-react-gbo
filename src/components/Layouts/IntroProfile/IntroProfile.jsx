f64b4db4458da286a55400f1b9559c0446ba821f
Gloria Bravo <gloria72159@hotmail.com>
2024-10-30 00:21:27 -0500

    ultimo-cambio

--- src/components/Layouts/IntroProfile/IntroProfile.jsx
+++ src/components/Layouts/IntroProfile/IntroProfile.jsx
@@ -1,13 +1,12 @@
 
 import React from 'react';
-import logo from '../../../assets/images/logo.mp4';
-import { ImgComponent } from '../../UI/LogoComponent/LogoComponent';
+
 
 export const IntroProfile = ({ src, alt }) => {
     return (
         <div className='container-img'>
-            <ImgComponent src={logo} alt='logo' />
-            
+
+
         </div>
     );
 };
